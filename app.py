0bdfcb458d0d52abfe2179e608c3163241670017
Nicholas Smith Renner <nrenner@seas.upenn.edu>
2019-10-24 15:20:18 -0400

    added xss, csrf

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 import subprocess
 from flask import Flask, render_template, request, redirect, session, url_for, escape
 import hashlib
+import bleach
 import os
+from flask_wtf.csrf import CSRFProtect
+
+
 
 app = Flask(__name__)
 
-app.secret_key = b'\x93:\x05iT\xa6\x8c\x18T\x88h\x01\xc6\xda\xf3\x19'
+app.secret_key = os.urandom(16)
+
+# using flask_wtf for csrf protection
+csrf = CSRFProtect(app)
 
 logins = {}
 
-DICTFILE = "static/wordlist.txt"
+DICTFILE = "wordlist.txt"
 
+# shashing our passwords!
 def hashit(key):
     m = hashlib.sha256()
     m.update(key.encode())
     return m.digest()
 
+# and checking them too!
 def checkit(hash2compare, key):
     return hash2compare == hashit(key)
 
@@ -23,7 +32,7 @@ def checkit(hash2compare, key):
 def validate_login(username, password, auth):
     try:
         if username not in logins: return 1
-
+        # differentiate via password and 2fa failure
         if not checkit(logins[username]['password'], password): return 1
 
         if not checkit(logins[username]['auth'], auth): return 2
@@ -37,8 +46,11 @@ def validate_login(username, password, auth):
 def register_login(username, password, auth):
 
     try:
+        # we want our usernames to not already be there, and we also want our fields to be populated!
         if username in logins: return 1
+        if not len(username) or not len(password) or not len(auth): return 1
 
+        # add user and hashed fields
         logins[username] = {}
         logins[username]['password'] = hashit(password)
         logins[username]['auth'] = hashit(auth)
@@ -52,70 +64,85 @@ def register_login(username, password, auth):
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    loggedin = False
+    if 'username' in session: loggedin = True
+    return render_template('home.html', loggedin=loggedin)
 
 @app.route("/register", methods=['POST', 'GET'])
 def register():
-    error = None
+    success = None
+    loggedin = False
+    if 'username' in session: loggedin = True
     if request.method == 'POST':
+        bleached_uname = bleach.clean(request.form['username'])
+        bleached_pass = bleach.clean(request.form['password'])
+        bleached_auth = bleach.clean(request.form['username'])
      
-        status = register_login(request.form['username'], request.form['password'], request.form['auth'])
+        status = register_login(bleached_uname, bleached_pass, bleached_auth)
         if status == 0:
-            return render_template('home.html')
+            success = 'Registration Success'
         elif status == 1:
-            error = 'Invalid Registration'
+            success = 'Error Invalid Registration'
         else:
-            error = 'System Error'
+            success = 'System Error'
      
-    return render_template('register.html', error=error)
+    return render_template('register.html', success=success, loggedin=loggedin)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    error = None
+    result = None
+    loggedin = False
+    if 'username' in session: loggedin = True
+
     if request.method == 'POST':
-        status = validate_login(request.form['username'], request.form['password'], request.form['auth'])
+        # bleach all input fileds to mediate XSS
+        bleached_uname = bleach.clean(request.form['username'])
+        bleached_pass = bleach.clean(request.form['password'])
+        bleached_auth = bleach.clean(request.form['username'])
+
+        status = validate_login(bleached_uname, bleached_pass, bleached_auth)
         if status == 0:
-            session['username'] = request.form['username']
-            return redirect('/spell_check')
-
+            result = 'Success'
+            session['username'] = bleached_uname
+            loggedin = True
         elif status == 1:
-            error = 'Invalid username/password'
+            result = 'Invalid username/password'
         elif status == 2:
-            error = '2fa'
+            result = '2fa'
         else:
-            error = 'System Error'
+            result = 'System Error'
 
-    return render_template('login.html', error=error)
+    return render_template('login.html', result=result, loggedin=loggedin)
 
 
 @app.route("/spell_check" , methods=['POST', 'GET'])
 def spell_check():
+    loggedin=False
+    # using flask 'session' for session hijacking
     if 'username' in session:
-        print("in spell check")
+        loggedin = True
         textout = None
         misspelled = None
-        if request.method == 'POST':
-            textout = request.form['inputtext']
-            print(request.form['inputtext'])
-            print("got textout" + str(textout))
 
-            textfile = 'static/textout.txt'
+        if request.method == 'POST':
+            textout = bleach.clean(request.form['inputtext'])
+ 
+            # we've got to write the text to a file for the checker to work (takes file input)
+            textfile = 'textout.txt'
             with open(textfile, 'w+') as f:
                 f.write(textout)
 
             # this subprocess call is mostly from the assignment one autograder
-            progout = subprocess.Popen(["./static/a.out", textfile, DICTFILE], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            progout = subprocess.Popen(["./a.out", textfile, DICTFILE], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             misspelled = progout.stdout.read().decode().strip().split('\n')
-
-            print(misspelled)
 
             f.close()
             os.remove(textfile)
         
-            return render_template('spell_check.html', textout=textout, misspelled=misspelled)
+            return render_template('spell_check.html', textout=textout, misspelled=misspelled, loggedin=loggedin)
         
         
-        return render_template('spell_check.html')
+        return render_template('spell_check.html', loggedin=loggedin)
 
 
     return redirect('/login')
@@ -123,7 +150,6 @@ def spell_check():
         
 @app.route('/logout')
 def logout():
-    # remove the username from the session if it's there
     session.pop('username', None)
     return render_template('home.html')
     
